2577b6d7d0575612aeb24da0483ae53b7094c410
Zack <zack.marron@ezasoft.com>
2011-02-16 18:55:00 -0500

    Project Added

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Padlock
+//
+//  Created by Zack Marron on 2/16/11.
+//  Copyright EZASoft 2011. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
